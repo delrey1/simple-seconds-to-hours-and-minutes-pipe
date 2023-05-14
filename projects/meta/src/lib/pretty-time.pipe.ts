d3d04bb26c2b7e1f52ec7a7a4a74d340f6345419
jorge <j@j>
2023-05-14 20:32:36 +0100

    Adding option to round hours

--- projects/meta/src/lib/pretty-time.pipe.ts
+++ projects/meta/src/lib/pretty-time.pipe.ts
@@ -9,15 +9,23 @@ export class PrettyTimePipe implements PipeTransform {
 
     let out = '';
 
-    const hours = Math.trunc(value / 60 / 60)
+    let hours = Math.trunc(value / 60 / 60)
     const minutes = Math.trunc((value / 60)) - (hours * 60)
+    const seconds = Math.trunc(value) - (hours * 60 * 60) - (minutes * 60)
 
 
-    if (hours > 0) {
-      out += hours + (options.hoursNotation ?? ' hour') + (hours != 1 && (options.appendS ?? true) ? 's' : '') + ' ';
+    if (hours > 0 || (options.roundNearestHour && minutes > 30)) {
+      if (options.roundNearestHour) {
+        hours = hours + ((minutes > 30) ? 1 : 0)
+        return (hours) + (options.hoursNotation ?? ' hour') + (hours != 1 && (options.appendS ?? true) ? 's ' : '') + ' '
+      }
+      out += hours + (options.hoursNotation ?? ' hour') + (hours != 1 && (options.appendS ?? true) ? 's ' : '') + ' ';
     }
     if (minutes > 0 && (options.showMinutes ?? true)) {
-      out += minutes + (options.minutesNotation ?? ' minute') + (minutes != 1 && (options.appendS ?? true) ? 's' : '');
+      out += minutes + (options.minutesNotation ?? ' minute') + (minutes != 1 && (options.appendS ?? true) ? 's ' : ' ');
+    }
+    if (seconds > 0 && (options.showSeconds ?? true)) {
+      out += seconds + (options.secondsNotation ?? ' second') + (seconds != 1 && (options.appendS ?? true) ? 's ' : ' ');
     }
 
     return out.trim();
@@ -31,6 +39,9 @@ export class PrettyTimePipe implements PipeTransform {
 export interface PrettyTimeOptions {
   minutesNotation?: string
   hoursNotation?: string
+  secondsNotation?: string
   showMinutes?: boolean
+  showSeconds?: boolean
   appendS?: boolean;
+  roundNearestHour?: boolean;
 }
